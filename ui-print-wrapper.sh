cd361b3beeb79caf91f48e34888b9b3b9e918ec0
Martin Jansa <Martin.Jansa@gmail.com>
2019-03-21 17:59:39 +0000

    updater-script: integrate ui-print-wrapper.sh to show some output while running webos_deploy.sh
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

---
+++ ui-print-wrapper.sh
@@ -0,0 +1,24 @@
+#!/sbin/busybox ash
+
+# Based on
+# https://forum.xda-developers.com/showthread.php?t=1023150
+# Changed the binary name, because in our case the update binary isn't called
+# update_binary as on the forum, nor update-binary as in our .zip file, but
+# just /tmp/updater, that's why the ps call never catched the right line and
+# correct FD
+# Also fix the test for empty OUTFD to use -n, otherwise it fails with
+# sh: : unknown operand
+# when it was really empty (because of missing quotes around $OUTFD)
+OUTFD=$(ps | grep -v "grep" | grep -o -E "updater(.*)" | cut -d " " -f 3);
+
+/sbin/busybox ash $* 2>&1 |
+
+while read -r line
+do
+    if [ -n "$OUTFD" ] ; then
+        echo "ui_print $line" >&$OUTFD;
+        echo "ui_print " >&$OUTFD;
+    else
+        echo "$line"
+    fi
+done
